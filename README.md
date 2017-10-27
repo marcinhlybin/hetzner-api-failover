56fd45cb66e89ad974e964e409f6e9a9ca1870ad
Marcin Hlybin <marcin.hlybin@gmail.com>
2017-10-27 17:31:31 +0200

    Brand new bullet-proof version with retries and forking

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
-# hetzner-api-failover
-Failover script for Hetzner API to be used with keepalived.
+# Keepalived Hetzner API
 
-Default configuration file path is /etc/keepalived/hetzner.conf. 
+Keepalived failover script for Hetzner API
 
-## Keepalived configuration:
+## Usage
+
+Copy `keepalived-hetzner.py` to `/usr/local/sbin/keepalived-hetzner.py`.
+Install pip packages from `requirements.txt`: `pip install -r requirements.txt`
 
 ```
-notify_master "/usr/local/sbin/hetzner.py"
+usage: keepalived-hetzner.py [-h] [--config CONFIG] [--exit] [--debug]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config CONFIG, -c CONFIG
+                        config file (default: /etc/keepalived/hetzner.conf)
+  --exit, -e            exit on error immediately (default: False)
+  --debug, -d           output messages to stdout (default: False)
 ```
+
+By default the script writes to syslog (`/var/log/syslog`). Use `--debug` to print message to stdout.
+
+Configuration file path is /etc/keepalived/hetzner.conf. Use `--config` to set different path.
+
+This software tries really hard to make failover. If it fails for whatever reason, it tries every minute again and again. It exits only if failover is confirmed or number of retries is hit (1000). Use `--exit` to exit immediately after error. Especially useful for debugging.
+
+If you spot any errors or uncaught exceptions please open an issue on github.
+
+## Configuration
+
+### /etc/keepalived/keepalived.conf
+
+Add following line to your `keepalived.conf`:
+
+```
+notify_master "/usr/local/sbin/keepalived-hetzner.py"
+```
+
+### /etc/keepalived/hetzner.conf
+
+See sample file `hetzner-sample.conf`.
+
+You can specify many failover addresses separated by comma in `failover_address` config option. The script will fork as many times as addresses you put there. Notice that you may exceed process limit or Hetzner's API requests limit.
---
+++ keepalived-hetzner.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# Keepalived failover script for Hetzner API
+#
+# Set in keepalived.conf:
+# notify_master "/usr/local/sbin/keepalived-hetzner.py"
+#
+# Marcin Hlybin, marcin.hlybin@gmail.com
+#
+import argparse
+import socket
+import os
+import sys
+import time
+import requests
+import json
+import syslog as s
+from configobj import ConfigObj
+
+CONFIG_FILE = '/etc/keepalived/hetzner.conf'
+RETRY_TIMES = 1000
+CONNECTION_TIMEOUT = 300
+STATUS_OK = 200
+STATUS_ALREADY_ROUTED = 409
+CONFIG_OPTIONS = (
+    'server_address',
+    'failover_address',
+    'api_url',
+    'api_user',
+    'api_password'
+)
+
+def log(message):
+    s.syslog(message)
+    if args.debug:
+        print message
+
+def read_config(config_file):
+    if not os.path.exists(config_file):
+        log("ERROR: Hetzner failover failed: config file '{}' not found".format(config_file))
+        sys.exit(1)
+
+    config = ConfigObj(config_file)
+
+    for option in CONFIG_OPTIONS:
+        if not config.get(option):
+            log("ERROR: Hetzner failover failed: option '{}' is required".format(option, config_file))
+            sys.exit(1)
+
+    try:
+        config['server_host'] = socket.gethostbyaddr(config['server_address'])[0]
+    except:
+        config['server_host'] = None
+
+    if type(config['failover_address']) is not list:
+        config['failover_address'] = [config['failover_address']]
+
+    return config
+
+def request(failover_address):
+    retry = 0
+    while retry < RETRY_TIMES:
+        params = { 'active_server_ip': c['server_address'] }
+        headers = { 'content-type': 'application/json' }
+        auth = (c['api_user'], c['api_password'])
+        url = c['api_url'] + '/failover/' + failover_address
+        try:
+            r = requests.post(url, params=params, headers=headers, auth=auth, timeout=CONNECTION_TIMEOUT)
+            if r.status_code == STATUS_OK:
+                log("Hetzner failover requested: {} -> {} [{}]".format(failover_address, c['server_address'], c['server_host']))
+                time.sleep(30)
+                continue
+            elif r.status_code == STATUS_ALREADY_ROUTED:
+                res = json.loads(r.content)
+                if res['error']['code'] == 'FAILOVER_ALREADY_ROUTED':
+                    log("Hetzner failover finished: {} -> {} [{}]".format(failover_address, c['server_address'], c['server_host']))
+                    break
+                else:
+                    log("Hetzner failover in progress: {} -> {} [{}]".format(failover_address, c['server_address'], c['server_host']))
+                    time.sleep(30)
+                    continue
+            else:
+                res = json.loads(r.content)
+                error_message = res.get('error', {}).get('message') or 'Connection error'
+                log("ERROR: Hetzner failover failed: {} -> {} [{}]: {}".format(failover_address, c['server_address'], c['server_host'], error_message))
+                if args.exit:
+                    # OS exit to not raise exception
+                    os._exit(1)
+                time.sleep(60)
+        except Exception, e:
+            log("ERROR: Hetzner failover failed: {} -> {} [{}]: Exception raised: {}".format(failover_address, c['server_address'], c['server_host'], str(e)))
+            if args.exit:
+                sys.exit(1)
+            time.sleep(60)
+            pass
+        retry += 1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config', '-c', dest='config', default=CONFIG_FILE, help='config file')
+    parser.add_argument('--exit', '-e', action='store_true', default=False, help='exit on error immediately')
+    parser.add_argument('--debug', '-d', action='store_true', default=False, help='output messages to stdout')
+    args = parser.parse_args()
+
+    c = read_config(args.config)
+
+    for failover_address in c['failover_address']:
+        try:
+            pid = os.fork()
+            if pid > 0:
+                request(failover_address)
+                break
+        except OSError, e:
+            log("ERROR: Hetzner failover failed: {} -> {} [{}]: Fork failed".format(failover_address, c['server_address'], c['server_host']))
+            sys.exit(1)
